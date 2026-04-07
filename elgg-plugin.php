5c22102b72355cebc5a72a037fdeeaf1dbf91a7b
ismayil.khayredinov@holoplot.com <ismayil.khayredinov@holoplot.com>
2026-04-07 15:59:22 +0200

    migrate(3.x): LLM-guided fixes for Elgg 3.x compatibility

---
+++ elgg-plugin.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+	'actions' => [
+		'modal_info/edit' => [
+			'access' => 'admin',
+		],
+		'modal_info/dismiss' => [],
+	],
+
+	'routes' => [
+		'collection:object:modal_info' => [
+			'path' => '/modal_info/all',
+			'resource' => 'modal_info/all',
+			'middleware' => [
+				\Elgg\Router\Middleware\AdminGatekeeper::class,
+			],
+		],
+		'add:object:modal_info' => [
+			'path' => '/modal_info/add',
+			'resource' => 'modal_info/add',
+			'middleware' => [
+				\Elgg\Router\Middleware\AdminGatekeeper::class,
+			],
+		],
+		'edit:object:modal_info' => [
+			'path' => '/modal_info/edit/{guid}',
+			'resource' => 'modal_info/edit',
+			'middleware' => [
+				\Elgg\Router\Middleware\AdminGatekeeper::class,
+			],
+		],
+		'view:object:modal_info' => [
+			'path' => '/modal_info/view/{guid}',
+			'resource' => 'modal_info/view',
+			'middleware' => [
+				\Elgg\Router\Middleware\AdminGatekeeper::class,
+			],
+		],
+	],
+];
